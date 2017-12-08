e8a02ec7c664ca6c88b280c02c93e64c1004fddc
silastsui <silastsuii@gmail.com>
2017-12-08 15:32:20 -0500

    Add solution to day 2

---
+++ 2.py
@@ -0,0 +1,34 @@
+def clean_sheet(sheet):
+    """
+    Args:
+        sheet (list): spreadsheet
+    """
+    return [[int(x) for x in line.strip().split()] for line in data]
+
+
+def dec2a(sheet):
+    """
+    Args:
+        sheet (list): parsed spreadsheet
+    """
+    total = 0
+    for line in data:
+        total += (max(line) - min(line))
+
+    return total
+
+def dec2b(sheet):
+    """
+    Args:
+        sheet (list): parsed spreadsheet
+    """
+    total = 0
+    for line in data:
+        for x in range(len(line)):
+            for y in range(x+1, len(line)):
+                if line[y] != 0 and line[x]%line[y] == 0:
+                    total += line[x]//line[y]
+                elif line[x] != 0 and line[y]%line[x] == 0:
+                    total += line[y]//line[x]
+
+    return total
